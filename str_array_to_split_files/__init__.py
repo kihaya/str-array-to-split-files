c46d2ce1d761b1381f5fb99d275192d6428fbfcb
Kihaya Sugiura <kihayas@gmail.com>
2020-10-30 12:52:58 +0900

    feat: Basic implementation

--- str_array_to_split_files/__init__.py
+++ str_array_to_split_files/__init__.py
@@ -1,3 +1,28 @@
+#coding: utf-8
 
-def str_array_to_split_files(data, target_directory):
-    return "hello"
+import os
+import typing
+import uuid
+
+def str_array_to_split_files(data=[], target_directory="") -> bool:
+    """
+    Split string array to splitted files.
+    """
+
+    ext = ".txt"
+
+    try:
+       if len(os.listdir(target_directory)) > 0: return False
+    except:
+       return False
+
+    if len(data) <= 0: return False
+
+    for str_data in data:
+        filename = str(uuid.uuid4())[:8]
+        full_file_name = target_directory + "/" + filename + ext
+        f = open(full_file_name, "w")
+        f.write(str_data)
+        f.close()
+
+    return True
