cd7364bdf94c3961c96db74325c00b236df2ba6a
Kihaya Sugiura <kihayas@gmail.com>
2020-10-30 12:13:34 +0900

    feat: Add initial files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="StrArray2SplitFiles",
+    version="1.0.0",
+    description="Split string array to files in specified directory",
+    author="Kihaya Sugiura",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ]
+)
---
+++ str_array_to_split_files/__init__.py
@@ -0,0 +1,3 @@
+
+def str_array_to_split_files(data, target_directory):
+    return "hello"
